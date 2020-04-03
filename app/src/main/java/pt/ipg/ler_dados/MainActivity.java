462549102626c1a3e7410529c74a6d9e3b80c17c
rui1199 <61785629+rui1199@users.noreply.github.com>
2020-04-03 23:07:28 +0100

    comit, fim do codigo, falta testar

--- app/src/main/java/pt/ipg/ler_dados/MainActivity.java
+++ app/src/main/java/pt/ipg/ler_dados/MainActivity.java
@@ -2,17 +2,42 @@ package pt.ipg.ler_dados;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE =  "pt.ipg.ler_dados.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void sendMessage(View view) {
+        Intent intent = new Intent(this, ecracommensagemfinal.class);
 
+        EditText editTextnome =(EditText) findViewById(R.id.editTextnome);
+        EditText editTexttele =(EditText) findViewById(R.id.editTexttele);
+        EditText editTextemail =(EditText) findViewById(R.id.editTextemail);
+        EditText editTextidade =(EditText) findViewById(R.id.editTextidade);
+        EditText editTextpeso =(EditText) findViewById(R.id.editTextpeso);
+        EditText editTextaltura =(EditText) findViewById(R.id.editTextaltura);
+
+        String mensagem1 = editTextnome.getText().toString();
+        String mensagem2 = editTexttele.getText().toString();
+        String mensagem3 = editTextemail.getText().toString();
+        String mensagem4 = editTextidade.getText().toString();
+        String mensagem5 = editTextpeso.getText().toString();
+        String mensagem6 = editTextaltura.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, mensagem1);
+        intent.putExtra(EXTRA_MESSAGE, mensagem2);
+        intent.putExtra(EXTRA_MESSAGE, mensagem3);
+        intent.putExtra(EXTRA_MESSAGE, mensagem4);
+        intent.putExtra(EXTRA_MESSAGE, mensagem5);
+        intent.putExtra(EXTRA_MESSAGE, mensagem6);
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/pt/ipg/ler_dados/ecracommensagemfinal.java
+++ app/src/main/java/pt/ipg/ler_dados/ecracommensagemfinal.java
@@ -2,7 +2,9 @@ package pt.ipg.ler_dados;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ecracommensagemfinal extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class ecracommensagemfinal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecracommensagemfinal);
+
+        Intent intent = getIntent();
+        String mensagem1 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String mensagem2 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String mensagem3 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String mensagem4 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String mensagem5 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String mensagem6 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textViewnome = findViewById(R.id.textViewnome);
+        textViewnome.setText(mensagem1);
+        TextView textViewtelefone = findViewById(R.id.textViewtelefone);
+        textViewnome.setText(mensagem2);
+        TextView textViewemail = findViewById(R.id.textView1email);
+        textViewnome.setText(mensagem3);
+        TextView textViewidade = findViewById(R.id.textViewidade);
+        textViewnome.setText(mensagem4);
+        TextView textViewnpeso = findViewById(R.id.textViewpeso);
+        textViewnome.setText(mensagem5);
+        TextView textViewaltura = findViewById(R.id.editTextaltura);
+        textViewnome.setText(mensagem6);
     }
 }
